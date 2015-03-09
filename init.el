49e6c802a2ebe1becc310ee458d41694ce221263
Ben Sanchez <mrsipan@gmail.com>
2015-03-09 15:33:19 -0400

    use evil-leader with evilnc

--- init.el
+++ init.el
@@ -133,7 +133,15 @@
 (evil-mode 1)
 
 (require 'evil-nerd-commenter)
-(evilnc-default-hotkeys)
+(evil-leader/set-key
+  "ci" 'evilnc-comment-or-uncomment-lines
+  "cl" 'evilnc-quick-comment-or-uncomment-to-the-line
+  "cc" 'evilnc-copy-and-comment-lines
+  "cp" 'evilnc-comment-or-uncomment-paragraphs
+  "cr" 'comment-or-uncomment-region
+  "cv" 'evilnc-toggle-invert-comment-line-by-line
+  "cc" 'evilnc-comment-operator)
+  ;;"\\" 'evilnc-comment-operator)
 
 (evil-leader/set-key "w" 'evil-write)
 
