5dee9d51a7dab045595240d375999ffacd978b64
Ben Sanchez <mrsipan@gmail.com>
2015-03-09 22:53:31 -0400

    style

--- init.el
+++ init.el
@@ -582,16 +582,16 @@
 ;; from http://goo.gl/qNgSo
 (setq desktop-globals-to-save
       (append '((extended-command-history . 30)
-                (file-name-history        . 100)
-                (grep-history             . 30)
-                (compile-history          . 30)
-                (minibuffer-history       . 50)
-                (query-replace-history    . 60)
-                (read-expression-history  . 60)
-                (regexp-history           . 60)
-                (regexp-search-ring       . 20)
-                (search-ring              . 20)
-                (shell-command-history    . 50)
+                (file-name-history . 100)
+                (grep-history . 30)
+                (compile-history . 30)
+                (minibuffer-history . 50)
+                (query-replace-history . 60)
+                (read-expression-history . 60)
+                (regexp-history . 60)
+                (regexp-search-ring . 20)
+                (search-ring . 20)
+                (shell-command-history . 50)
                 tags-file-name
                 register-alist)))
 
