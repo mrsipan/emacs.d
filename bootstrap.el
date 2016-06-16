feb1d648b08fa5b14db8dff14c3405f60ffb06a6
Ben Sanchez <ben.sanchez@rallyhealth.com>
2016-06-16 14:51:57 -0400

    Make tmp directory

--- bootstrap.el
+++ bootstrap.el
@@ -3,5 +3,10 @@
 (if (file-exists-p "el-get-install.el")
     (delete-file "el-get-install.el"))
 
+(let ((dir-name "tmp"))
+  (unless (file-exists-p dir-name)
+    (print "Make tmp directory")
+    (make-directory dir-name)))
+
 (url-copy-file "https://raw.githubusercontent.com/dimitri/el-get/master/el-get-install.el" "el-get-install.el")
 (load-file "el-get-install.el")
