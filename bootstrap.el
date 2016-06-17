c772fdd4ed296121c7da6813c08dd0bbb1dbb57f
Ben Sanchez <bensanchez@mbprally.home>
2016-06-17 00:43:57 -0400

    Make auto-saves directory

--- bootstrap.el
+++ bootstrap.el
@@ -3,10 +3,13 @@
 (if (file-exists-p "el-get-install.el")
     (delete-file "el-get-install.el"))
 
-(let ((dir-name "tmp"))
+(let ((dir-name "tmp/auto-saves"))
   (unless (file-exists-p dir-name)
     (print "Make tmp directory")
-    (make-directory dir-name)))
+    (make-directory dir-name t)))
 
-(url-copy-file "https://raw.githubusercontent.com/dimitri/el-get/master/el-get-install.el" "el-get-install.el")
+(url-copy-file
+ "https://raw.githubusercontent.com/dimitri/el-get/master/el-get-install.el"
+ "el-get-install.el")
+
 (load-file "el-get-install.el")
